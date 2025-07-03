19fcf88e19365546466197c45d04b45709d3af9f
John A. Samuel <144686157+CodeGallantX@users.noreply.github.com>
2025-07-03 17:15:56 +0100

    Updates

--- src/components/layout/DashboardSidebar.jsx
+++ src/components/layout/DashboardSidebar.jsx
@@ -52,7 +52,7 @@ const DashboardSidebar = () => {
         <div className="p-6 border-b border-sidebar-border">
           <div className="flex items-center gap-3">
             <div className="w-8 h-8 bg-bronze rounded-lg flex items-center justify-center">
-              <Plane className="w-5 h-5 text-black" />
+              <img src="https://tango-oscar.vercel.app/logo.png" alt="logo">
             </div>
             {!collapsed && (
               <div>
