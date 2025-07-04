60fc185770c44e03ada9c57d7b444d3e11ee19ac
John A. Samuel <144686157+CodeGallantX@users.noreply.github.com>
2025-07-04 21:10:41 +0100

    updates

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,7 @@ export default {
 		},
 		extend: {
 			fontFamily: {
-				'bricolage': ['Bricolage Grotesque', 'sans-serif'],
+				'outfit': ['Outfit', 'sans-serif'],
 			},
 			colors: {
 				border: 'hsl(var(--border))',
